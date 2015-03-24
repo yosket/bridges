8f2d8f49f821702e85ac12bd1a93a7b74c3b1dbd
Yosuke Tanaka <yosket87@gmail.com>
2015-03-25 01:53:49 +0900

    再帰処理に挑むもうまくいかず

--- public/js/app.js
+++ public/js/app.js
@@ -3,66 +3,80 @@
 
 	var app = angular.module('app', []);
 
-	app.factory('Website', ['$http', function($http) {
-		var _domain, _pages, _message;
-		var _init = function(domain) {
-			_domain = domain;
-			_pages = [];
-			_message = '';
-		};
-		var _check = function(url) {
-			_pages.push({
-				url: url,
-				enabled: null
-			});
-			_message = '通信中 ...';
-			$http.get('api/', { params: { url: url } }).then(function(response) {
-				var data = response.data;
-				if (data.status) {
-					angular.forEach(_pages, function(page) {
-						if (page.url === url) {
-							page.enabled = true;
-						}
+	app.factory('Website', function($q, $http) {
+		return {
+			domain: '',
+			checkList: [],
+			pages: [],
+			message: '',
+			checkStart: function(url) {
+				var parser = new URL(url);
+				this.domain = parser.origin;
+				this.checkList.push(url);
+				// while (this.checkList.length > 0) {
+				for (var i = 0; i < 3; i++) {
+					var t = this;
+					console.log(this.checkList[i]);
+					$q.all([this.check(this.checkList[i])]).then(function() {
+						// angular.forEach(t.checkList, function(value, key) {
+						// 	if (value === url) {
+						// 		t.checkList.splice(key, 1);
+						// 	}
+						// });
+						t.checkList.splice(i, 1);
+						console.log(t.checkList.length);
 					});
-					angular.forEach(data.hrefs, function(href) {
-						var isDuplicated = false;
-						angular.forEach(_pages, function(page) {
-							if (href === page.url) {
-								isDuplicated = true;
+				}
+			},
+			check: function(url) {
+				var d = $q.defer();
+				var currentPage = {
+					url: url,
+					inner: []
+				};
+				this.message = url + 'を調査中 ...';
+				var t = this;
+				$http.get('api/', { params: { url: url } }).then(function(response) {
+					var data = response.data;
+					if (data.status) {
+						angular.forEach(data.hrefs, function(href) {
+							if (!isDuplicated(href, currentPage.inner)) {
+								currentPage.inner.push({
+									url: href,
+									enabled: null
+								});
+							}
+							if (href.indexOf(t.domain) === 0 && !isDuplicated(href, t.checkList)) {
+								t.checkList.push(href);
 							}
 						});
-						if (!isDuplicated) {
-							_pages.push({
-								url: href,
-								enabled: null
-							});
-						}
-					});
-					_message = '';
-				} else {
-					_message = data.message;
-				}
-			});
+						t.pages.push(currentPage);
+						t.message = '';
+					} else {
+						t.message = data.message;
+					}
+					d.resolve();
+				});
+				return d.promise;
+			}
 		};
-		return {
-			domain: _domain,
-			pages: _pages,
-			message: _message,
-			init: _init,
-			check: _check
-		};
-	}]);
+	});
 
 	app.controller('AppController', ['$scope', 'Website', function AppController($scope, Website) {
 		$scope.Website = Website;
 		$scope.check = function() {
-			var parser = new URL($scope.query);
-			Website.init(parser.origin);
-			Website.check($scope.query);
-		};
-		$scope.secondCheck = function() {
-			Website.check($scope.secondQuery);
+			Website.checkStart($scope.query);
 		};
 	}]);
+
+	var isDuplicated = function(str, array) {
+		var duplicated = false;
+		angular.forEach(array, function(value) {
+			if (value === str) {
+				duplicated = true;
+			}
+		});
+		return duplicated;
+	}
 
 })();
